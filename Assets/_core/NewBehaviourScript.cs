5af252372d8e23483f29c8de1fe9fd5f23486a50
oywq2020 <2603321762@qq.com>
2022-11-10 14:06:43 +0800

    complete the login demo

--- Assets/_core/NewBehaviourScript.cs
+++ Assets/_core/NewBehaviourScript.cs
@@ -7,7 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.SetString("Name","欧阳");
         
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("Name"));
+       
     }
 
     // Update is called once per frame
