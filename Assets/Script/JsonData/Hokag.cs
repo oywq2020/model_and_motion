f727c6cf6ce7661a7b59b18f2bc3ee88055dbebb
oywq2020 <2603321762@qq.com>
2022-11-17 09:28:12 +0800

    ads

---
+++ Assets/Script/JsonData/Hokag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hokag 
+{
+   public string Name { get; set; }
+   public int Age { get; set; }
+   public string Skill { get; set; }
+   
+   public override string ToString()
+   {
+      return $"姓名：{Name}，年龄：{Age}，技能：{Skill}";
+   }
+}
---
+++ Assets/Script/XMLController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class XMLController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("sss");
+        XmlDocument xml = new XmlDocument();
+        
+        //read xml file
+        xml.Load(Application.dataPath+"/Xml/Hokags.xml");
+        
+        //get the root node of xml file
+        XmlNode root = xml.LastChild;
+        
+        //get primary node of root
+        var rootChildNodes = root.ChildNodes;
+
+        List<XmlElement> xmlElements = new List<XmlElement>();
+        foreach (XmlElement hokag in rootChildNodes)
+        {
+            xmlElements.Add(hokag);
+        }
+
+        Debug.Log("sss");
+        foreach (var VARIABLE in xmlElements)
+        {
+            Debug.Log("sss");
+            Debug.Log(VARIABLE);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
