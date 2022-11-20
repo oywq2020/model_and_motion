d686d8138fb5a45794dcd8ca863ee981aba8cf94
oywq2020 <2603321762@qq.com>
2022-11-20 10:47:45 +0800

    complete lly model magica cloth

--- Assets/Script/XMLController.cs
+++ Assets/Script/XMLController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Xml;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class XMLController : MonoBehaviour
@@ -8,30 +11,74 @@ public class XMLController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("sss");
+        // Debug.Log("sss");
+        // XmlDocument xml = new XmlDocument();
+        //
+        // //read xml file
+        // xml.Load(Application.dataPath + "/Xml/Hokags.xml");
+        //
+        // //get the root node of xml file
+        // XmlNode root = xml.LastChild;
+        //
+        //
+        // //get primary node of root
+        // var rootChildNodes = root.ChildNodes;
+        //
+        //
+        // List<XmlElement> xmlElements = new List<XmlElement>();
+        //
+        // foreach (var rootChildNode in rootChildNodes)
+        // {
+        //     xmlElements.Add(rootChildNode as XmlElement);
+        // }
+        //
+        // foreach (var VARIABLE in rootChildNodes)
+        // {
+        //     Debug.Log(VARIABLE);
+        // }
+        //
+        // foreach (var element in xmlElements)
+        // {
+        //
+        //     Hokag hokag = new Hokag();
+        //     hokag.ID = element.GetAttribute("id");
+        //     hokag.Name = element.ChildNodes[0].InnerText;
+        //     hokag.Age = Int32.Parse(element.ChildNodes[1].InnerText);
+        //     hokag.Skill = element.ChildNodes[2].InnerText;
+        //     
+        //     Debug.Log(hokag);
+        // }
+
         XmlDocument xml = new XmlDocument();
         
-        //read xml file
-        xml.Load(Application.dataPath+"/Xml/Hokags.xml");
+        //create declaration
+        xml.AppendChild(xml.CreateXmlDeclaration("1.0", "UTF-8", null));
         
-        //get the root node of xml file
-        XmlNode root = xml.LastChild;
+        //create root element
+        xml.AppendChild(xml.CreateElement("Hokags"));
         
-        //get primary node of root
-        var rootChildNodes = root.ChildNodes;
+        //create root node
+        XmlNode root = xml.SelectSingleNode("Hokags");
+        //add element
+        var hokagInfo = xml.CreateElement("HokagInfo");
 
-        List<XmlElement> xmlElements = new List<XmlElement>();
-        foreach (XmlElement hokag in rootChildNodes)
-        {
-            xmlElements.Add(hokag);
-        }
+        //set attribute
+        hokagInfo.SetAttribute("id", "1");
 
-        Debug.Log("sss");
-        foreach (var VARIABLE in xmlElements)
-        {
-            Debug.Log("sss");
-            Debug.Log(VARIABLE);
-        }
+        var nameElement = xml.CreateElement("Name");
+        var ageelement = xml.CreateElement("Age");
+        var skillElement1 = xml.CreateElement("Skill");
+
+        nameElement.InnerText = "鸣人";
+        ageelement.InnerText = "21";
+        skillElement1.InnerText = "嘴遁";
+
+        root.AppendChild(hokagInfo);
+        hokagInfo.AppendChild(nameElement);
+        hokagInfo.AppendChild(ageelement);
+        hokagInfo.AppendChild(skillElement1);
+        
+        xml.Save(Application.dataPath+"/Xml/newHokags.xml");
     }
 
     // Update is called once per frame
